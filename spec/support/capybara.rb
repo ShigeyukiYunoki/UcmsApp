8ca83f09a3f3f769f0c1104c4db4b75689ffca99
shige8 <shigeyuki.ynk@gmail.com>
2021-09-11 16:08:08 +0900

    Merge branch 'circleci' of https://github.com/shige8/UcmsApp1 into circleci

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,9 @@
 Capybara.javascript_driver = :selenium_chrome
 
 RSpec.configure do |config|
-  # config.before(:each, type: :system) do
-  #   driven_by :rack_test
-  # end
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
 
   config.before(:each, type: :system, js: true) do
     if ENV["SELENIUM_DRIVER_URL"].present?
