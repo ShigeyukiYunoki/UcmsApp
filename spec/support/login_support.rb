ce74c995d7b9ee2a90f7f1711565ecfe5786c186
shige8 <shigeyuki.ynk@gmail.com>
2021-09-11 22:23:52 +0900

    def sign_in_as(user),def create_post(post)をcapybara.rbにまとめる

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,13 +1,13 @@
-module LoginSupport
-  def sign_in_as(user)
-    visit login_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    find('#login').click
-    find('.swal-button--gotit').click
-  end
-end
-
-RSpec.configure do |config|
-  config.include LoginSupport
-end
+# module LoginSupport
+#   def sign_in_as(user)
+#     visit login_path
+#     fill_in 'Email', with: user.email
+#     fill_in 'Password', with: user.password
+#     find('#login').click
+#     find('.swal-button--gotit').click
+#   end
+# end
+#
+# RSpec.configure do |config|
+#   config.include LoginSupport
+# end
