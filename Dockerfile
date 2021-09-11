b85629429bbe9d468241a8e381eb67444668fb60
shige8 <shigeyuki.ynk@gmail.com>
2021-09-11 21:46:05 +0900

    capybara.rb修正

--- Dockerfile
+++ Dockerfile
@@ -10,17 +10,17 @@ apt-get install -yq build-essential libpq-dev vim postgresql-client
 RUN apt-get install -y nodejs npm && npm install n -g && n 14.16.1
 
 # chromeの追加
-RUN apt-get update && apt-get install -y unzip && \
-    CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-    wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/ && \
-    unzip ~/chromedriver_linux64.zip -d ~/ && \
-    rm ~/chromedriver_linux64.zip && \
-    chown root:root ~/chromedriver && \
-    chmod 755 ~/chromedriver && \
-    mv ~/chromedriver /usr/bin/chromedriver && \
-    sh -c 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' && \
-    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
-    apt-get update && apt-get install -y google-chrome-stable
+# RUN apt-get update && apt-get install -y unzip && \
+#     CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
+#     wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/ && \
+#     unzip ~/chromedriver_linux64.zip -d ~/ && \
+#     rm ~/chromedriver_linux64.zip && \
+#     chown root:root ~/chromedriver && \
+#     chmod 755 ~/chromedriver && \
+#     mv ~/chromedriver /usr/bin/chromedriver && \
+#     sh -c 'wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' && \
+#     sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
+#     apt-get update && apt-get install -y google-chrome-stable
 
 
 RUN mkdir /app_name
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,15 +1,17 @@
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+Capybara.register_driver :remote_chrome do |app|
+  url = ENV['SELENIUM_DRIVER_URL']
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => [
+        'no-sandbox',
+        'headless',
+        'disable-gpu',
+        'window-size=1680,1050'
+      ]
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, desired_capabilities: caps)
 end
-
-Capybara.javascript_driver = :selenium_chrome
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
@@ -17,28 +19,55 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    if ENV["SELENIUM_DRIVER_URL"].present?
-      driven_by :selenium, using: :chrome, options: {
-        browser: :remote,
-        #web コンテナ側からselenium_chromeコンテナのchromeを使用するために、
-        #optionsの中で、urlにSELENIUM_DRIVER_URL環境変数の値を設定
-        url: ENV.fetch("SELENIUM_DRIVER_URL"),
-        desired_capabilities: :chrome
-      }
-    else
-      driven_by :selenium_chrome_headless
-      # headless chromeからはローカル環境ではなくwebコンテナ側のrailsアプリを表示してテストする必要があるため
-      Capybara.server_host = 'web'
-      Capybara.app_host='http://web'
-    end
+    #driven_by :selenium_chrome_headless
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.app_host = "http://#{Capybara.server_host}"
+    driven_by :remote_chrome
   end
 end
 
-def full_title(page_title = '')
-  base_title = 'UcmsApp'
-  if page_title.empty?
-    base_title
-  else
-    "#{page_title}|#{base_title}"
-  end
-end
+
+# Capybara.register_driver :chrome_headless do |app|
+#   options = ::Selenium::WebDriver::Chrome::Options.new
+#
+#   options.add_argument('--headless')
+#   options.add_argument('--no-sandbox')
+#   options.add_argument('--disable-dev-shm-usage')
+#   options.add_argument('--window-size=1400,1400')
+#
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+#
+# Capybara.javascript_driver = :selenium_chrome
+#
+# RSpec.configure do |config|
+#   config.before(:each, type: :system) do
+#     driven_by :rack_test
+#   end
+#
+#   config.before(:each, type: :system, js: true) do
+#     if ENV["SELENIUM_DRIVER_URL"].present?
+#       driven_by :selenium, using: :chrome, options: {
+#         browser: :remote,
+#         #web コンテナ側からselenium_chromeコンテナのchromeを使用するために、
+#         #optionsの中で、urlにSELENIUM_DRIVER_URL環境変数の値を設定
+#         url: ENV.fetch("SELENIUM_DRIVER_URL"),
+#         desired_capabilities: :chrome
+#       }
+#     else
+#       driven_by :selenium_chrome_headless
+#       # headless chromeからはローカル環境ではなくwebコンテナ側のrailsアプリを表示してテストする必要があるため
+#       Capybara.server_host = 'web'
+#       Capybara.app_host='http://web'
+#     end
+#   end
+# end
+#
+# def full_title(page_title = '')
+#   base_title = 'UcmsApp'
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{page_title}|#{base_title}"
+#   end
+# end
