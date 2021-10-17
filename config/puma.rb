9c2e1de5c14965d05a99ce6552de78740902b672
shige8 <shigeyuki.ynk@gmail.com>
2021-10-17 12:43:54 +0900

    app_root = File.expand_path("../../../..", __FILE__)

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ threads min_threads_count, max_threads_count
 
 # socketの設定
 # bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-app_root = File.expand_path("../../..", __FILE__)
+app_root = File.expand_path("../../../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
