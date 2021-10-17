73d1af1ec2c75e2a1cdf658b0f13760bd6055523
shige8 <shigeyuki.ynk@gmail.com>
2021-10-17 16:43:51 +0900

    puma_return,nginx.conf_return

--- config/puma.rb
+++ config/puma.rb
@@ -13,12 +13,12 @@ threads min_threads_count, max_threads_count
 # port        ENV.fetch('PORT') { 3000 }
 
 # socketの設定
-# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-app_dir = File.expand_path("../..", __FILE__)
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# app_dir = File.expand_path("../..", __FILE__)
+# bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+# pidfile "#{app_dir}/tmp/pids/puma.pid"
+# state_path "#{app_dir}/tmp/pids/puma.state"
+# stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Specifies the `environment` that Puma will run in.
 #
