062d0a4d3af978bd02f297bde89dd18c76ed8321
shige8 <shigeyuki.ynk@gmail.com>
2021-10-17 12:12:53 +0900

    app_root = File.expand_path("../..", __FILE__)追加とpuma -C config/puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,10 @@ threads min_threads_count, max_threads_count
 # port        ENV.fetch('PORT') { 3000 }
 
 # socketの設定
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
 # Specifies the `environment` that Puma will run in.
 #
