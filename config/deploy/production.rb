8ac8021e1b2f4a6652eb05b25c01c51e1ef68c25
shige8 <shigeyuki.ynk@gmail.com>
2021-10-31 15:07:37 +0900

    set :ssh_options, {   keys: %w[~/.ssh/d_rsa_[Fingerprintの:がない文字列]]

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -36,7 +36,7 @@ server '35.73.129.65', user: 'shige888', roles: %w[web nginx]
 #
 # Global options
 set :ssh_options, {
-  keys: %w[~/.ssh/id_rsa],
+  keys: %w[~/.ssh/id_rsa_5059b82d5dbd5a27431768c916ab35f5],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
