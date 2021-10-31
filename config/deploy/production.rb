898f46e5ecd7b64aef66fd6775ec32cc4fe1ea0d
shige8 <shigeyuki.ynk@gmail.com>
2021-10-31 18:31:40 +0900

    only_branch=master,keys: %w[~/.ssh/id_rsa_18e...

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -36,6 +36,7 @@ server '35.73.129.65', user: 'shige888', roles: %w[web nginx]
 #
 # Global options
 set :ssh_options, {
+  # Docker上で認識され、CapistranoがサーバへSSHログイン可能になり、サーバへデプロイされる
   keys: %w[~/.ssh/id_rsa_18e06f026f3295a2041d45b3f3a9e9a1],
   forward_agent: true,
   auth_methods: %w[publickey]
