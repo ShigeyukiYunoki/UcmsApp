7a151903d4c23a5d8667022e1bc634524e77215e
shige8 <shigeyuki.ynk@gmail.com>
2021-11-21 12:02:31 +0900

    add_gif,add_@max_days_in_medicine_helper,fix_top0_about_user_show

--- app/helpers/medicines_helper.rb
+++ app/helpers/medicines_helper.rb
@@ -7,24 +7,24 @@ module MedicinesHelper
 
     return unless logged_in?
 
-      @tookmedicinelast = current_user.medicines.where.not(took_medicine_at: nil).last
-      if @tookmedicinelast
-        @tooklastat = @tookmedicinelast.took_medicine_at
-        gon.medicine = @tooklastat
-      end
+    @tookmedicinelast = current_user.medicines.where.not(took_medicine_at: nil).last
+    if @tookmedicinelast
+      @tooklastat = @tookmedicinelast.took_medicine_at
+      gon.medicine = @tooklastat
+    end
 
-      @medicineslast = current_user.medicines.last
-      if @medicineslast && current_user.medicines.where.not(gonna_take_medicine_at: nil).last
-        @gonnatake = @medicineslast.gonna_take_medicine_at.strftime('%H:%M')
-        gon.gonnatake = @gonnatake
-      end
+    @medicineslast = current_user.medicines.last
+    if @medicineslast && current_user.medicines.where.not(gonna_take_medicine_at: nil).last
+      @gonnatake = @medicineslast.gonna_take_medicine_at.strftime('%H:%M')
+      gon.gonnatake = @gonnatake
+    end
 
-      @no = current_user.medicines.where(took_medicine_at: nil).last
-      gon.noexist = @no
-      # if @no
-      #   @noid = @no.id
-      #   gon.noid = @noid
-      # end
+    @no = current_user.medicines.where(took_medicine_at: nil).last
+    gon.noexist = @no
+    # if @no
+    #   @noid = @no.id
+    #   gon.noid = @noid
+    # end
   end
 
   def tookmedicines(user)
@@ -33,38 +33,43 @@ module MedicinesHelper
 
   def took_medicine_days_straight(user)
     return unless logged_in?
-      @days = 0
-      # "took_medicine_at"を昇順で取得
-      @tookmedicinedays = user.medicines.order(took_medicine_at: :asc).pluck('took_medicine_at')
-      # 順番に２つごとに並べて日付を比較
-      @tookmedicinedays.each_cons(2) do |f, s|
-        # sがnilだと比較できずエラーになる
-        if !s.nil?
-          @fdt = DateTime.strptime(f, '%Y年%m月%d日')
-          @sdt = DateTime.strptime(s, '%Y年%m月%d日')
-          # ２つの日付を比較し連続なら＋１そうでないなら０
-          if @fdt + 1 == @sdt
-            @days += 1
-          else
-            @days = 0
-          end
-        # gonna_take_medicine_atの値を持ち、took_medicine_atがnilのデータが最後に存在する場合
-        # 最後はsがnilとなり比較できないので問答無用で＋１
-        else
+
+    @days = 0
+    @max_days = 0
+    # "took_medicine_at"を昇順で取得
+    @tookmedicinedays = user.medicines.order(took_medicine_at: :asc).pluck('took_medicine_at')
+    # 順番に２つごとに並べて日付を比較
+    @tookmedicinedays.each_cons(2) do |f, s|
+      # sがnilだと比較できずエラーになる
+      if !s.nil?
+        @fdt = DateTime.strptime(f, '%Y年%m月%d日')
+        @sdt = DateTime.strptime(s, '%Y年%m月%d日')
+        # ２つの日付を比較し連続なら＋１そうでないなら０
+        if @fdt + 1 == @sdt
           @days += 1
+          @max_days = @days if @max_days < @days
+        else
+          @days = 0
+          # @max_days += 1
         end
+      # gonna_take_medicine_atの値を持ち、took_medicine_atがnilのデータが最後に存在する場合
+      # 最後はsがnilとなり比較できないので問答無用で＋１
+      else
+        @days += 1
+        @max_days += 1
       end
-      # gonna_take_medicine_atの値を持ち、took_medicine_atがnilのデータが最後に存在しない場合
-      # 最後のs自体存在せず比較できないのでこちらも問答無用で＋１
-      @days += 1 unless @tookmedicinedays.last.nil?
+    end
+    # gonna_take_medicine_atの値を持ち、took_medicine_atがnilのデータが最後に存在しない場合
+    # 最後のs自体存在せず比較できないのでこちらも問答無用で＋１
+    @days += 1 unless @tookmedicinedays.last.nil?
+    @max_days += 1 unless @tookmedicinedays.last.nil?
+    # if @tookmedicinedays.count == 1
+    #   @max_days = 1
+    # end
 
-      # if @tookmedicinedays.count == 1
-      #   @days = 1
-      # end
-
-      # if @tookmedicinedays.blank?
-      #   @days = 0
-      # end
+    # if @tookmedicinedays.blank?
+    #   @days = 0
+    # end
   end
 
   # def took_medicine_days_straight
