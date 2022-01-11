c228f7539ae56af56855b47edd1fad59c5d92174
shige8 <shigeyuki.ynk@gmail.com>
2022-01-11 18:15:59 +0900

    delete_spec_medmailer

--- spec/mailers/medicine_mailer_spec.rb
+++ spec/mailers/medicine_mailer_spec.rb
@@ -1,15 +1,15 @@
-require "rails_helper"
-
-RSpec.describe MedicineMailer, type: :mailer do
-  include ActiveJob::TestHelper
-  describe "take_medicine_email" do
-    let(:user) { FactoryBot.create(:user) }
-    let(:mail) { MedicineMailer.take_medicine(user) }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("服薬の時間です")
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["noreply@www.ucmsapp.com"])
-    end
-  end
-end
+# require "rails_helper"
+#
+# RSpec.describe MedicineMailer, type: :mailer do
+#   include ActiveJob::TestHelper
+#   describe "take_medicine_email" do
+#     let(:user) { FactoryBot.create(:user) }
+#     let(:mail) { MedicineMailer.take_medicine(user) }
+#
+#     it "renders the headers" do
+#       expect(mail.subject).to eq("服薬の時間です")
+#       expect(mail.to).to eq([user.email])
+#       expect(mail.from).to eq(["noreply@www.ucmsapp.com"])
+#     end
+#   end
+# end
